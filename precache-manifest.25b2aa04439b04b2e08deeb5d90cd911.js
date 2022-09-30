9fa44aa2e0e843d018f54b4a7ab1e58f99480fda
jesjonesc <58461507+yrshva@users.noreply.github.com>
2022-10-01 00:20:35 +0100

    Updates

---
+++ precache-manifest.25b2aa04439b04b2e08deeb5d90cd911.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "65c844b62aac15b93e56",
+    "url": "/static/css/main.e7b90f12.chunk.css"
+  },
+  {
+    "revision": "65c844b62aac15b93e56",
+    "url": "/static/js/main.65c844b6.chunk.js"
+  },
+  {
+    "revision": "bb0898e875365f106adb",
+    "url": "/static/js/1.bb0898e8.chunk.js"
+  },
+  {
+    "revision": "76e5b42dcd0b6249ad4f",
+    "url": "/static/css/2.03962285.chunk.css"
+  },
+  {
+    "revision": "76e5b42dcd0b6249ad4f",
+    "url": "/static/js/2.76e5b42d.chunk.js"
+  },
+  {
+    "revision": "de8e62e958aa5b967bf0",
+    "url": "/static/js/runtime~main.de8e62e9.js"
+  },
+  {
+    "revision": "9494dd46c73a60187eaaf5f118e6f90c",
+    "url": "/static/media/weather-app.9494dd46.png"
+  },
+  {
+    "revision": "5021c8fb243e31e647b91db731c37a4a",
+    "url": "/static/media/dictionary-app.5021c8fb.png"
+  },
+  {
+    "revision": "a7e1ff93657924fb36daa91809afbc10",
+    "url": "/static/media/mobile-dictionary.a7e1ff93.mp4"
+  },
+  {
+    "revision": "da63c58804d5ff7f5a389d61c055af1b",
+    "url": "/index.html"
+  }
+];
